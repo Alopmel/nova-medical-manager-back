a7149f4e1857c5ddecf7fbbb4aec545d99f5f3b4
Alopmel <78558722+Alopmel@users.noreply.github.com>
2023-06-09 23:38:28 +0200

    Connect with MongoDB and insert test user

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,3 +1,22 @@
-export const register = (req, res) => res.send("register");
+import User from "../models/user.model.js";
+
+export const register = async (req, res) => {
+    const {email, password, username} = req.body;
+    
+    try {
+        const newUser = new User({
+            username,
+            email,
+            password
+        });
+        
+        const userSaved = await newUser.save();
+        res.json(userSaved);  
+    } catch (error) {
+        console.log(error)
+    }
+
+    
+};
 
 export const login = (req, res) => res.send("login");
