977e45b63f807795d9aff8c7bf289fbf79c66167
Alopmel <78558722+Alopmel@users.noreply.github.com>
2023-06-12 22:50:30 +0200

    Installing depenencies for ecrypt the password

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,13 +1,16 @@
 import User from "../models/user.model.js";
+import bcrypt from "bcryptjs";
 
 export const register = async (req, res) => {
     const {email, password, username} = req.body;
     
     try {
+        const passwordHash = await bcrypt.hash(password, 10) // Encriptar la contraseña
+        
         const newUser = new User({
             username,
             email,
-            password
+            password: passwordHash, 
         });
         
         const userSaved = await newUser.save();
