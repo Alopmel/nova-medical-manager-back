fa4b6d2bbd5754099831aa423d1ba574aa3f324d
Alopmel <78558722+Alopmel@users.noreply.github.com>
2023-06-23 21:23:06 +0200

    "user record created

--- src/app.js
+++ src/app.js
@@ -1,12 +1,16 @@
 import express from 'express';
 import morgan from "morgan";
 import cookieParser from 'cookie-parser';
+import cors from 'cors';
 
 import authRoutes from './routes/auth.routes.js';
 import taskRoutes from './routes/task.router.js';
 
 const app = express();
 
+app.use(cors({
+    origin: 'http://127.0.0.1:5173',
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(cookieParser());
