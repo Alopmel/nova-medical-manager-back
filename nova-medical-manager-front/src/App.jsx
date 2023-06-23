4d16fbe7f8f612c29269c1267a4f9f0f54545ac0
Alopmel <78558722+Alopmel@users.noreply.github.com>
2023-06-23 20:55:42 +0200

    Added register and login page

--- nova-medical-manager-front/src/App.jsx
+++ nova-medical-manager-front/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import RegisterPage from "./pages/RegisterPage";
+import LoginPage from "./pages/LoginPage";
+
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/' element={<h1>Home Page</h1>} />
-        <Route path='/login' element={<h1>Login</h1>} />
-        <Route path='/registro' element={<h1>Registro</h1>} />
+        <Route path='/login' element={<LoginPage />} />
+        <Route path='/registro' element={<RegisterPage />} />
         <Route path='/quiensoy' element={<h1>¿Quién Soy?</h1>} />
         <Route path='/servicios' element={<h1>Servicios</h1>} />
         <Route path='/perfil' element={<h1>Perfil</h1>} />
